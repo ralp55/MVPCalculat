6d6140d989edde033011b05d660eeed46edd3f8a
ralp55 <gabenvav@gmail.com>
2025-06-15 11:26:44 +0400

    init commit

---
+++ src/test/java/neo/project/task/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package neo.project.task.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
